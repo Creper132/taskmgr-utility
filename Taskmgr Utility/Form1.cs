12d22a3820f3dc28517d5b91f683880836d923fe
Szymon <77246300+Creper132@users.noreply.github.com>
2021-06-22 14:08:22 +0200

    Dodaj pliki projektów.

---
+++ Taskmgr Utility/Form1.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Taskmgr_Utility
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            RegistryKey objRegistryKey = Registry.CurrentUser.CreateSubKey(@"Software\Microsoft\Windows\CurrentVersion\Policies\System");
+            objRegistryKey.DeleteValue("DisableTaskMgr");
+            objRegistryKey.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            RegistryKey objRegistryKey = Registry.CurrentUser.CreateSubKey(@"Software\Microsoft\Windows\CurrentVersion\Policies\System");
+            if (objRegistryKey.GetValue("DisableTaskMgr") == null)
+                objRegistryKey.SetValue("DisableTaskMgr", "1");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Process.Start("taskkill", "/F /IM Taskmgr.exe /T");
+        }
+    }
+}
